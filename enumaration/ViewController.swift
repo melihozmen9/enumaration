00956605dd64d5b3cc9bb1d7993afe0379db2f4e
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-06-10 14:36:15 +0300

    enumaration1

--- enumaration/ViewController.swift
+++ enumaration/ViewController.swift
@@ -11,9 +11,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+        let bugun = gunler.cumartesi
+        print(bugun)
+        
+        switch bugun {
+        case gunler.pazartesi:
+            print("haftanın ilk iş günü")
+        case gunler.sali:
+            print("haftanın ikinci iş günü")
+        case gunler.cumartesi:
+            print("haftasonu")
+        case gunler.pazar:
+            print("yarın iş var")
+        default:
+            print("lütfen bir gün giriniz")
+        }
     }
-
+   
+    
 
 }
-
+// haftanın günlerini enum ile tanımlayalım. :)
+enum gunler {
+    case pazartesi
+    case sali
+    case carsamba
+    case persembe
+    case cuma
+    case cumartesi
+    case pazar
+    
+}
+// kısa tanımlama
+enum gunlerkisa {
+    case pazartesi,sali, carsamba,persembe,cuma,cumartesi,pazar
+}
